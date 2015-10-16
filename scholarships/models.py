6cdd2ccfaef9b6899c9c5ea2f191aa77a67f1647
pygeek <palanshagarwal@gmail.com>
2015-10-16 21:05:14 +0530

    database fields

--- scholarships/models.py
+++ scholarships/models.py
@@ -22,32 +22,32 @@ class interest (models.Model):
         return self.interest_name
 
 class caste (models.Model):
-	caste_id=models.AutoField(primary_key=True)
-	caste_name=models.CharField(max_length=250,default=' ')
+    caste_id=models.AutoField(primary_key=True)
+    caste_name=models.CharField(max_length=250,default=' ')
 
-	def __unicode__(self):
-		return self.caste_name
+    def __unicode__(self):
+        return self.caste_name
 
 class level(models.Model):
-	level_id=models.AutoField(primary_key=True)
-	level_name=models.CharField(max_length=250,default=' ')
+    level_id=models.AutoField(primary_key=True)
+    level_name=models.CharField(max_length=250,default=' ')
 
-	def __unicode__(self):
-		return self.level_name
+    def __unicode__(self):
+        return self.level_name
 
 class state(models.Model):
-	state_id=models.AutoField(primary_key=True)
-	state_name=models.CharField(max_length=250,default=' ')
+    state_id=models.AutoField(primary_key=True)
+    state_name=models.CharField(max_length=250,default=' ')
 
-	def __unicode__(self):
-		return self.state_name
+    def __unicode__(self):
+        return self.state_name
 
 class religion(models.Model):
-	religion_id=models.AutoField(primary_key=True)
-	religion_name=models.CharField(max_length=250,default=' ')
+    religion_id=models.AutoField(primary_key=True)
+    religion_name=models.CharField(max_length=250,default=' ')
 
-	def __unicode__(self):
-		return self.religion_name
+    def __unicode__(self):
+        return self.religion_name
 
 class abroad (models.Model):
     abroad_id=models.AutoField(primary_key=True)
@@ -55,91 +55,95 @@ class abroad (models.Model):
     # new class method
     def __unicode__(self):
         return self.abroad_name
+class scholarship_emails(models.Model):
+    email_address = models.EmailField(blank=True)
 
 class scholarship(models.Model):
-	scholarship_id=models.AutoField(primary_key=True)
-	name=models.CharField(max_length=250,default=' ')
-	offered_by=models.CharField(max_length=250,default=' ')
-	total_number_scholarship=models.IntegerField(default=0)
+    scholarship_id=models.AutoField(primary_key=True)
+    name=models.CharField(max_length=250,default=' ')
+    offered_by=models.CharField(max_length=250,default=' ')
+    total_number_scholarship=models.IntegerField(default=0)
+    image_url = models.URLField(max_length=200, default=' ')
+    summary = models.TextField(blank=True)
+    
+    # foreign keys
+    emails = models.ManyToManyField(scholarship_emails)
+    education_field=models.ManyToManyField(field)
+    education_interest=models.ManyToManyField(interest,blank=True)
+    education_caste=models.ManyToManyField(caste)
+    education_religion=models.ManyToManyField(religion)
+    education_level=models.ManyToManyField(level)
+    education_state=models.ManyToManyField(state)
+    education_abroad=models.ManyToManyField(abroad)
 
-	# foreign keys
+    # other filters
 
-	education_field=models.ManyToManyField(field)
-	education_interest=models.ManyToManyField(interest,blank=True)
-	education_caste=models.ManyToManyField(caste)
-	education_religion=models.ManyToManyField(religion)
-	education_level=models.ManyToManyField(level)
-	education_state=models.ManyToManyField(state)
-	education_abroad=models.ManyToManyField(abroad)
+    gender=models.IntegerField(default=0)
+    disability=models.IntegerField(default=0)
+    income=models.IntegerField(default=0)
 
-	# other filters
+    #deadline details
 
-	gender=models.IntegerField(default=0)
-	disability=models.IntegerField(default=0)
-	income=models.IntegerField(default=0)
+    deadline=models.DateTimeField(blank=True,default=timezone.now())
+    deadline_details=models.TextField(default=' ')
+    deadline_type=models.IntegerField(default=0)
 
-	#deadline details
+    # amount
+    amount_frequency=models.IntegerField(default=0)
+    amount_period=models.IntegerField(default=0)
+    amount=models.IntegerField(default=0)
+    other_benefits=models.TextField(default=' ')
+    currency=models.IntegerField(default=0)
 
-	deadline=models.DateTimeField(blank=True,default=timezone.now())
-	deadline_details=models.TextField(default=' ')
-	deadline_type=models.IntegerField(default=0)
+    # type govt,trust,competitions
 
-	# amount
-	amount_frequency=models.IntegerField(default=0)
-	amount_period=models.IntegerField(default=0)
-	amount=models.IntegerField(default=0)
-	other_benefits=models.TextField(default=' ')
-	currency=models.IntegerField(default=0)
+    scholarship_type=models.IntegerField(default=0)
 
-	# type govt,trust,competitions
+    # time
 
-	scholarship_type=models.IntegerField(default=0)
+    timestamp=models.DateTimeField(default=timezone.now(),blank=True)
 
-	# time
+    # application mode of scholarship
 
-	timestamp=models.DateTimeField(default=timezone.now(),blank=True)
+    application_mode=models.IntegerField(default=0)
 
-	# application mode of scholarship
+    eligibility=models.TextField(default=' ')
+    about=models.TextField(default=' ')
+    procedure=models.TextField(default=' ')
+    contact_details=models.TextField(default=' ')
+    apply_link=models.CharField(max_length=500,default=' ')
+    additional_links=models.CharField(max_length=500,default=' ')
 
-	application_mode=models.IntegerField(default=0)
+    meta_data=models.CharField(max_length=200,default=' ')
+    meta_title=models.CharField(max_length=65,default=' ')
 
-	eligibility=models.TextField(default=' ')
-	about=models.TextField(default=' ')
-	procedure=models.TextField(default=' ')
-	contact_details=models.TextField(default=' ')
-	apply_link=models.CharField(max_length=500,default=' ')
-	additional_links=models.CharField(max_length=500,default=' ')
+    def __unicode__ (self):
+        return self.name;
 
-	meta_data=models.CharField(max_length=170,default=' ')
-	meta_title=models.CharField(max_length=65,default=' ')
-
-	def __unicode__ (self):
-		return self.name;
-
-	def get_absolute_url(self):
-		x = re.sub('[^A-Za-z0-9]+','-',re.sub('[^A-Za-z0-9]+',' ',self.name).strip(' '))
-		return "/scholarship-details/"+x ;
+    def get_absolute_url(self):
+        x = re.sub('[^A-Za-z0-9]+','-',re.sub('[^A-Za-z0-9]+',' ',self.name).strip(' '))
+        return "/scholarship-details/"+x ;
 
 class UserProfile(models.Model):
-	# linking user profile to a user
-	user = models.OneToOneField(User, related_name='profile')
-	auth_type = models.CharField(max_length=200,blank=False,default=None)
-	#additional user information
-	user_state = models.ForeignKey(state)
-	user_level = models.ForeignKey(level)
-	user_caste = models.ForeignKey(caste)
-	user_religion = models.ForeignKey(religion)
-	user_income = models.CharField(max_length=200,blank=False)
-	user_interest=models.ManyToManyField(interest,blank=True)
-	user_type = models.CharField(max_length=200,blank=True)
-	user_abroad = models.ForeignKey(abroad)
-	user_field = models.ForeignKey(field)
-	user_gender = models.IntegerField(blank=False)
-	user_disability = models.IntegerField(blank=False)
+    # linking user profile to a user
+    user = models.OneToOneField(User, related_name='profile')
+    auth_type = models.CharField(max_length=200,blank=False,default=None)
+    #additional user information
+    user_state = models.ForeignKey(state)
+    user_level = models.ForeignKey(level)
+    user_caste = models.ForeignKey(caste)
+    user_religion = models.ForeignKey(religion)
+    user_income = models.CharField(max_length=200,blank=False)
+    user_interest=models.ManyToManyField(interest,blank=True)
+    user_type = models.CharField(max_length=200,blank=True)
+    user_abroad = models.ForeignKey(abroad)
+    user_field = models.ForeignKey(field)
+    user_gender = models.IntegerField(blank=False)
+    user_disability = models.IntegerField(blank=False)
 
 
-	def __unicode__(self):
-		return self.user.username
+    def __unicode__(self):
+        return self.user.username
 
 
 
@@ -148,4 +152,4 @@ class UserProfile(models.Model):
 class MyModelAdmin(admin.ModelAdmin):
     formfield_overrides = {
         models.ManyToManyField: {'widget': CheckboxSelectMultiple},
-	}
+    }
