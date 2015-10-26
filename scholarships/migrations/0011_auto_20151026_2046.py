31657121369d8ff981e1014f34f2dd5b554c3c60
shashank <shashanks.1903@gmail.com>
2015-10-26 23:39:38 +0530

    change password only basic

---
+++ scholarships/migrations/0011_auto_20151026_2046.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import migrations, models
+import datetime
+from django.utils.timezone import utc
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('scholarships', '0010_auto_20151026_1550'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='scholarship',
+            name='deadline',
+            field=models.DateTimeField(default=datetime.datetime(2015, 10, 26, 15, 16, 26, 173000, tzinfo=utc), blank=True),
+        ),
+        migrations.AlterField(
+            model_name='scholarship',
+            name='timestamp',
+            field=models.DateTimeField(default=datetime.datetime(2015, 10, 26, 15, 16, 26, 174000, tzinfo=utc), blank=True),
+        ),
+    ]
